659fdf8a0d14668b4c11abfb04f4d09f5e4d56e1
Tahsin Gül <tahsingul09@gmail.com>
2023-02-14 00:41:09 +0300

    routing paths are added to approuting module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NotFoundComponent } from './site/notfound/notfound.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './site/signin/signin.component';
+import { HomeComponent } from './site/home/home.component';
+import { AuthGuard } from './core/auth-guard.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home', component: HomeComponent,
+    canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
+    canLoad: [AuthGuard],
+  },
+  { path: 'signin', component: SignInComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
