18b115ef12b03b0a7d3e0f3642b99b09014e3b5e
Tahsin Gül <tahsingul09@gmail.com>
2023-02-14 00:43:20 +0300

    components under the site path added to app module's declarations. ReactiveFormsModule for signin page and HttpClientModule for RestAPI request added to imports. SessionService who is provided at root vas added to appModules providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
+import { SessionService } from './core/session.service';
+import { HomeComponent } from './site/home/home.component';
+import { SignInComponent } from './site/signin/signin.component';
+import { NotFoundComponent } from './site/notfound/notfound.component';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    SignInComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [SessionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
